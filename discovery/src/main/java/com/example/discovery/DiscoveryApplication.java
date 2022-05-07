5ed9e3339b57775f352f8c7a083a616ebec03fde
Ulisses Rosa <ulisses.skysect@hotmail.com>
2022-05-07 03:49:14 -0300

    Discovery and Management working well in eureka

--- discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -2,10 +2,12 @@ package com.example.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+
 @EnableEurekaServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
--- management-service/src/main/java/com/example/managementservice/ManagementServiceApplication.java
+++ management-service/src/main/java/com/example/managementservice/ManagementServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.managementservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"com.example.core.model"})
 @EnableJpaRepositories({"com.example.core.repositories"})
+@EnableEurekaClient
 public class ManagementServiceApplication {
 
     public static void main(String[] args) {
