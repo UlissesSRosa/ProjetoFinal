cdb9c450dc4747c747c1f1df018582b5c3521c74
Matheus Rizzo <matheus.l.rizzo@gmail.com>
2022-05-21 14:53:21 -0300

    Termino Estrutura RPA Envia Promoções

--- management-service/src/main/java/com/example/managementservice/domains/scheduled/SendCustomerEmailPromotion.java
+++ management-service/src/main/java/com/example/managementservice/domains/scheduled/SendCustomerEmailPromotion.java
@@ -38,8 +38,8 @@ public class SendCustomerEmailPromotion {
 		for(int i = 0; i < 5; i++) {
 			Map<String, String> produto = new HashMap<>();
 			produto.put("nome", "produto"+i);
-			produto.put("desc", "descricao"+i);
-			produto.put("image", "url"+i);
+			produto.put("preco", "10"+i);
+			produto.put("image", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/280px-Red_Apple.jpg");
 			produtos.add(produto);
 		}
 		
@@ -96,15 +96,60 @@ public class SendCustomerEmailPromotion {
 	}
 	
 	private String buildHtml(List<Map<String, String>> produtos) {
-		log.info("Montando HTML length produtos: " + produtos.size());
+		log.info("Montando HTML. Produtos em Promoção: " + produtos.size());
+		
+		StringBuilder html = new StringBuilder();
+		html.append(montainicioHtml());
+		
 		produtos.forEach(produto -> {
-			log.info("nome" + produto.get("nome"));
-			log.info("desc" + produto.get("desc"));
-			log.info("image" + produto.get("image"));
+			html.append(montaMeioHtml(produto));
 		});
 		
-		return "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /></head><body><b>Testeeeeeeeeeee</b></body></html>\r\n"
-				+ "";
+		html.append(montafimHtml());
+		log.info("Html Email: " + html.toString());
+		return html.toString();
+	}
+
+	private String montainicioHtml() {
+		StringBuilder htmlInicio = new StringBuilder();
+		htmlInicio.append("<!DOCTYPE html><html lang=\"pt-br\"><head><!-- Meta tags Obrigatórias --><meta charset=\"utf-8\" /> ");
+		htmlInicio.append("<meta name=\"viewport\"content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/> ");
+		htmlInicio.append("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" ");
+		htmlInicio.append("integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\"/> ");
+		htmlInicio.append("<style> .flex-center {display: flex; justify-content: center;}</style></head> ");
+		htmlInicio.append("<body><div class=\"container\"><div class=\"row hidden-sm-up mt-2\"><h4 class=\"text-left\"><b><i>Caro cliente,</i></b></h4></div> ");
+		htmlInicio.append("<div class=\"row hidden-sm-up\"><p class=\"text-5xl\"><b><i>Aproveite as nossas ofertas de hoje:</i></b></p></div> ");
+		htmlInicio.append("<div class=\"row hidden-sm-up\"> ");
+		return htmlInicio.toString();
+	}
+	
+	private Object montaMeioHtml(Map<String, String> produto) {
+		StringBuilder htmlMeio = new StringBuilder();
+		htmlMeio.append("<div class=\"col-md-4 mt-2\"><div class=\"card \" style=\"width: 18rem\"> ");
+		htmlMeio.append("<img class=\"card-img-top\" src=\"$URLIMAGEM\" alt=\"100rem\"/> <div class=\"card-body\"> ");
+		htmlMeio.append("<h5 class=\"card-title\">$NOMEPRODUTO</h5> <p class=\"card-text\">$PRECO</p> ");
+		htmlMeio.append("<a href=\"#\" class=\"btn btn-primary\">Visitar</a> </div></div></div> ");
+		String htmlMeioEdit = htmlMeio.toString();
+		htmlMeioEdit = htmlMeioEdit.replace("$URLIMAGEM", produto.get("image"));
+		htmlMeioEdit = htmlMeioEdit.replace("$NOMEPRODUTO", produto.get("nome"));
+		htmlMeioEdit = htmlMeioEdit.replace("$PRECO", produto.get("preco"));
+		return htmlMeioEdit;
+	}
+	
+	private String montafimHtml() {
+		StringBuilder htmlFim = new StringBuilder();
+		htmlFim.append("</div><div class=\"row hidden-sm-up mt-3\"><p class=\"text-5xl\"> ");
+		htmlFim.append("<b><i>Atenciosamente,<br>Tech Store</i></b></p></div></div></div> ");
+		htmlFim.append("<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" ");
+		htmlFim.append("integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" ");
+		htmlFim.append("crossorigin=\"anonymous\"></script> ");
+		htmlFim.append("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" ");
+		htmlFim.append("integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" ");
+		htmlFim.append("crossorigin=\"anonymous\"></script><script ");
+		htmlFim.append("src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" ");
+		htmlFim.append("integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" ");
+		htmlFim.append("crossorigin=\"anonymous\"></script></body></html> ");
+		return htmlFim.toString();
 	}
 	
 }
